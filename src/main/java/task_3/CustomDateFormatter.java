b27e77c713d82169be1b98cf0a98d8c9a1bf5204
Maria <ronios@yandex.ru>
2022-12-25 11:37:47 +0500

    correct parse formatter

--- src/main/java/task_3/CustomDateFormatter.java
+++ src/main/java/task_3/CustomDateFormatter.java
@@ -1,8 +1,15 @@
 package task_3;
 
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 public class CustomDateFormatter {
-    public static final DateTimeFormatter PARSE_FORMATTER = DateTimeFormatter.ofPattern("[dd.MM.yyyy][dd.MM.yy][dd/MM/yyyy][dd/MM/yy]");
+    //public static final DateTimeFormatter PARSE_FORMATTER = DateTimeFormatter.ofPattern("[dd.MM.yyyy][dd.MM.yy][dd/MM/yyyy][dd/MM/yy]");
     public static final DateTimeFormatter PRINT_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yy");
+
+    public static final DateTimeFormatter PARSE_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("[dd.MM.][dd.MM.][dd/MM/][dd/MM/]")
+            .appendValueReduced(ChronoField.YEAR_OF_ERA, 2, 4, 1971)
+            .toFormatter();
 }
